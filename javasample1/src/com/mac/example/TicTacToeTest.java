ae1f09a60e0ca47cbc1b1d35d752dd03e281d7f6
MiguelConnor <miguelaxa@gmail.com>
2013-01-01 00:20:49 -0800

    add new test class

---
+++ javasample1/src/com/mac/example/TicTacToeTest.java
@@ -0,0 +1,31 @@
+package com.mac.example;
+
+import junit.framework.TestCase;
+
+public class TicTacToeTest extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testMousePressed() {
+		System.out.println("pass");
+		//fail("Not yet implemented");
+	}
+
+	public void testInitBoard() {
+		System.out.println("pass");
+		//fail("Not yet implemented");
+	}
+
+}
